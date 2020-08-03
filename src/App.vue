0a807afd6a606f233c64fe2ff87b581548773938
rainbowsuger <807748386@qq.com>
2020-08-03 08:55:18 +0800

    vite

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <HelloWorld msg="Hello Vue 3.0 + Vite">
+    <template #><h2>this is a slot</h2></template>
+    <template v-slot:center="{data}">
+      <div class="block-flex">
+        <div v-for="item in data" class="mar-right10">{{item}}</div>
+      </div>
+    </template>
+  </HelloWorld>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+/**
+* 感知到的变化：
+* 1、每个.vue文件可以不用一个dom元素包裹。
+* 2、v-slot父元素中可以用#代替，data数据解构，用法见app.vue.
+*/
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <slot></slot>
+  <slot name="center" :data="data"></slot>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0,
+      data: ['left', 'center', 'right']
+    }
+  }
+}
+</script>
